486f7f5794909b9ba50589ba558aa5883d0fd1b1
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2024-07-03 16:28:33 +0100

    refactor: copy method into all subclasses

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -64,7 +64,17 @@ class AppreciateWithAgeUpdater(SpecialCaseUpdater):
 
 
 class AgedBrieUpdater(AppreciateWithAgeUpdater):
-    pass
+    def adjust_quality_post_sell_date(item):
+        if item.name != "Aged Brie":
+            if item.name != "Backstage passes to a TAFKAL80ETC concert":
+                if item.quality > 0:
+                    if item.name != "Sulfuras, Hand of Ragnaros":
+                        item.quality = item.quality - 1
+            else:
+                item.quality = item.quality - item.quality
+        else:
+            if item.quality < 50:
+                item.quality = item.quality + 1
 
 
 class BackstagePassUpdater(AppreciateWithAgeUpdater):
