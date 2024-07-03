18e2d9b8266f3518b28686d3745c3f9487e32879
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2024-07-03 17:17:55 +0100

    refactor: extract constant: MAX_QUALITY

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+MAX_QUALITY = 50
+
 
 class GildedRose(object):
     def __init__(self, items):
@@ -23,14 +25,14 @@ class DefaultUpdater:
             if item.quality > 0:
                 item.quality = item.quality - 1
         else:
-            if item.quality < 50:
+            if item.quality < MAX_QUALITY:
                 item.quality = item.quality + 1
                 if item.name == "Backstage passes to a TAFKAL80ETC concert":
                     if item.sell_in < 11:
-                        if item.quality < 50:
+                        if item.quality < MAX_QUALITY:
                             item.quality = item.quality + 1
                     if item.sell_in < 6:
-                        if item.quality < 50:
+                        if item.quality < MAX_QUALITY:
                             item.quality = item.quality + 1
         item.sell_in = item.sell_in - 1
         if item.sell_in < 0:
@@ -41,7 +43,7 @@ class DefaultUpdater:
                 else:
                     item.quality = item.quality - item.quality
             else:
-                if item.quality < 50:
+                if item.quality < MAX_QUALITY:
                     item.quality = item.quality + 1
 
 
