544630e417cd9a265a9310516de066ea38a90615
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2024-07-03 18:05:43 +0100

    refactor: BackstagePassUpdater overrides new_quality()

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -50,21 +50,17 @@ class AgedBrieUpdater(DefaultUpdater):
             return item.quality + 2
 
 
-class BackstagePassUpdater:
-    def change_all(item):
-        if item.quality < MAX_QUALITY:
-            item.quality = item.quality + 1
-
-            if item.quality < MAX_QUALITY:
-                if item.sell_in < 11:
-                    item.quality = item.quality + 1
-                if item.sell_in < 6:
-                    item.quality = item.quality + 1
-
-        item.sell_in = item.sell_in - 1
-
+class BackstagePassUpdater(DefaultUpdater):
+    @classmethod
+    def new_quality(cls, item):
         if item.sell_in < 0:
-            item.quality = item.quality - item.quality
+            return 0
+        elif item.sell_in < 5:
+            return item.quality + 3
+        elif item.sell_in < 10:
+            return item.quality + 2
+        else:
+            return item.quality + 1
 
 
 class Item:
