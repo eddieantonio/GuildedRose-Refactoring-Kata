d46d9f3fee766c3436e9e0663c6360076a2e9b9e
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2024-07-03 19:44:45 +0100

    test: basic test for .update_quality()

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -5,11 +5,13 @@ from gilded_rose import GildedRose, Item
 
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
+    def test_ordinary_item(self):
+        items = [Item("Plain ol' Grog", 1, 10)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEqual("foo", items[0].name)
+        self.assertEqual("Plain ol' Grog", items[0].name)
+        self.assertEqual(0, items[0].sell_in)
+        self.assertEqual(9, items[0].quality)
 
 
 if __name__ == "__main__":
