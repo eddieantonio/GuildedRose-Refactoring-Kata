5ca87900bd66b16ca34c837b54541e405d969a58
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2024-07-03 17:26:58 +0100

    refactor: change conditional with polymorphism: BackstagePassUpdater

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -12,39 +12,21 @@ class GildedRose(object):
             updater = {
                 "Sulfuras, Hand of Ragnaros": LegendaryItemUpdater,
                 "Aged Brie": AgedBrieUpdater,
+                "Backstage passes to a TAFKAL80ETC concert": BackstagePassUpdater,
             }.get(item.name, DefaultUpdater)
             updater.change_all(item)
 
 
 class DefaultUpdater:
     def change_all(item):
-        if (
-            item.name == "Aged Brie"
-            or item.name == "Backstage passes to a TAFKAL80ETC concert"
-        ):
-            if item.quality < MAX_QUALITY:
-                item.quality = item.quality + 1
-                if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                    if item.sell_in < 11:
-                        if item.quality < MAX_QUALITY:
-                            item.quality = item.quality + 1
-                    if item.sell_in < 6:
-                        if item.quality < MAX_QUALITY:
-                            item.quality = item.quality + 1
-        else:
+        if item.quality > 0:
+            item.quality = item.quality - 1
+
+        item.sell_in = item.sell_in - 1
+
+        if item.sell_in < 0:
             if item.quality > 0:
                 item.quality = item.quality - 1
-        item.sell_in = item.sell_in - 1
-        if item.sell_in < 0:
-            if item.name == "Aged Brie":
-                if item.quality < MAX_QUALITY:
-                    item.quality = item.quality + 1
-            else:
-                if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                    if item.quality > 0:
-                        item.quality = item.quality - 1
-                else:
-                    item.quality = item.quality - item.quality
 
 
 class LegendaryItemUpdater:
@@ -63,6 +45,24 @@ class AgedBrieUpdater:
         if item.sell_in < 0:
             if item.quality < MAX_QUALITY:
                 item.quality = item.quality + 1
+
+
+class BackstagePassUpdater:
+    def change_all(item):
+        if item.quality < MAX_QUALITY:
+            item.quality = item.quality + 1
+
+            if item.sell_in < 11:
+                if item.quality < MAX_QUALITY:
+                    item.quality = item.quality + 1
+            if item.sell_in < 6:
+                if item.quality < MAX_QUALITY:
+                    item.quality = item.quality + 1
+
+        item.sell_in = item.sell_in - 1
+
+        if item.sell_in < 0:
+            item.quality = item.quality - item.quality
 
 
 class Item:
