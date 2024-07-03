1e249c64e2f5fb6c418d06ebfff85b73e72664b8
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2024-07-03 19:50:29 +0100

    test: add charmed test

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -13,6 +13,14 @@ class GildedRoseTest(unittest.TestCase):
         self.assertEqual(0, items[0].sell_in)
         self.assertEqual(9, items[0].quality)
 
+    def test_charmed_item(self):
+        items = [Item("Charmed Grog", 1, 10)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual("Charmed Grog", items[0].name)
+        self.assertEqual(0, items[0].sell_in)
+        self.assertEqual(8, items[0].quality)
+
 
 if __name__ == "__main__":
     unittest.main()
