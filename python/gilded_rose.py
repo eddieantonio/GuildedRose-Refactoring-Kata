3ceae23f126c0011b78fa1878bd535e7c819c800
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2024-07-03 17:23:05 +0100

    refactor: invert conditional

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -19,12 +19,9 @@ class GildedRose(object):
 class DefaultUpdater:
     def change_all(item):
         if (
-            item.name != "Aged Brie"
-            and item.name != "Backstage passes to a TAFKAL80ETC concert"
+            item.name == "Aged Brie"
+            or item.name == "Backstage passes to a TAFKAL80ETC concert"
         ):
-            if item.quality > 0:
-                item.quality = item.quality - 1
-        else:
             if item.quality < MAX_QUALITY:
                 item.quality = item.quality + 1
                 if item.name == "Backstage passes to a TAFKAL80ETC concert":
@@ -34,6 +31,9 @@ class DefaultUpdater:
                     if item.sell_in < 6:
                         if item.quality < MAX_QUALITY:
                             item.quality = item.quality + 1
+        else:
+            if item.quality > 0:
+                item.quality = item.quality - 1
         item.sell_in = item.sell_in - 1
         if item.sell_in < 0:
             if item.name == "Aged Brie":
