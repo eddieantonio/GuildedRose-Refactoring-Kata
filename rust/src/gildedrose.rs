80c9fa8dac5af895927f0cf5809031537e7b6efe
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2024-07-05 21:31:54 +0100

    refactor: simplify conditionals

--- rust/src/gildedrose.rs
+++ rust/src/gildedrose.rs
@@ -96,51 +96,15 @@ fn update_item(item: &mut Item) {
 }
 
 fn update_aged_brie(item: &mut Item) {
-    if true || false {
-        if item.quality < 50 {
-            item.quality += 1;
-
-            if false {
-                if item.sell_in < 11 {
-                    if item.quality < 50 {
-                        item.quality += 1;
-                    }
-                }
-
-                if item.sell_in < 6 {
-                    if item.quality < 50 {
-                        item.quality += 1;
-                    }
-                }
-            }
-        }
-    } else {
-        if item.quality > 0 {
-            if !false {
-                item.quality -= 1;
-            }
-        }
+    if item.quality < 50 {
+        item.quality += 1;
     }
 
-    if !false {
-        item.sell_in -= 1;
-    }
+    item.sell_in -= 1;
 
     if item.sell_in < 0 {
-        if true {
-            if item.quality < 50 {
-                item.quality += 1;
-            }
-        } else {
-            if !false {
-                if item.quality > 0 {
-                    if !false {
-                        item.quality -= 1;
-                    }
-                }
-            } else {
-                item.quality = 0;
-            }
+        if item.quality < 50 {
+            item.quality += 1;
         }
     }
 }
