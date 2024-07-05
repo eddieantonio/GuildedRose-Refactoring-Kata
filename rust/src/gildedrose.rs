9c06809212078033ac339213b11f6b1eb6a8131f
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2024-07-05 22:06:11 +0100

    refactor: extract parts

--- rust/src/gildedrose.rs
+++ rust/src/gildedrose.rs
@@ -46,17 +46,10 @@ fn update_item(item: &mut Item) {
         return;
     }
 
+    // Before sell_in changes...
     if is_aged_brie {
         if item.quality < 50 {
             item.quality += 1;
-        }
-
-        item.sell_in -= 1;
-
-        if item.sell_in < 0 {
-            if item.quality < 50 {
-                item.quality += 1;
-            }
         }
     } else if is_backstage_pass {
         if item.quality < 50 {
@@ -74,19 +67,26 @@ fn update_item(item: &mut Item) {
                 }
             }
         }
-
-        item.sell_in -= 1;
-
-        if item.sell_in < 0 {
-            item.quality = 0;
-        }
     } else {
         if item.quality > 0 {
             item.quality -= 1;
         }
+    }
 
-        item.sell_in -= 1;
+    item.sell_in -= 1;
 
+    // After sell_in changes...
+    if is_aged_brie {
+        if item.sell_in < 0 {
+            if item.quality < 50 {
+                item.quality += 1;
+            }
+        }
+    } else if is_backstage_pass {
+        if item.sell_in < 0 {
+            item.quality = 0;
+        }
+    } else {
         if item.sell_in < 0 {
             if item.quality > 0 {
                 item.quality -= 1;
