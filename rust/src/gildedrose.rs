24433bbb01efa721b8d7abda03486f91c91ef4a0
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2024-07-05 21:41:16 +0100

    refactor: simply logic (is_backstage_pass == true)

--- rust/src/gildedrose.rs
+++ rust/src/gildedrose.rs
@@ -104,49 +104,26 @@ fn update_item(item: &mut Item) {
 }
 
 fn update_backstage_pass(item: &mut Item) {
-    let is_backstage_pass = true;
-    let is_legendary = false;
+    if item.quality < 50 {
+        item.quality += 1;
 
-    if is_backstage_pass {
-        if item.quality < 50 {
-            item.quality += 1;
-
-            if is_backstage_pass {
-                if item.sell_in < 11 {
-                    if item.quality < 50 {
-                        item.quality += 1;
-                    }
-                }
-
-                if item.sell_in < 6 {
-                    if item.quality < 50 {
-                        item.quality += 1;
-                    }
-                }
+        if item.sell_in < 11 {
+            if item.quality < 50 {
+                item.quality += 1;
             }
         }
-    } else {
-        if item.quality > 0 {
-            if !is_legendary {
-                item.quality -= 1;
+
+        if item.sell_in < 6 {
+            if item.quality < 50 {
+                item.quality += 1;
             }
         }
     }
 
-    if !is_legendary {
-        item.sell_in -= 1;
-    }
+    item.sell_in -= 1;
 
     if item.sell_in < 0 {
-        if !is_backstage_pass {
-            if item.quality > 0 {
-                if !is_legendary {
-                    item.quality -= 1;
-                }
-            }
-        } else {
-            item.quality = 0;
-        }
+        item.quality = 0;
     }
 }
 
