f8596f4ea07eb0ef0f88e2864e5ab0333c8a06d6
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2024-07-05 21:51:41 +0100

    refactor: make consistent

--- rust/src/gildedrose.rs
+++ rust/src/gildedrose.rs
@@ -54,7 +54,6 @@ fn update_item(item: &mut Item) {
                 item.quality += 1;
             }
         }
-        return;
     } else if is_backstage_pass {
         if item.quality < 50 {
             item.quality += 1;
@@ -77,20 +76,19 @@ fn update_item(item: &mut Item) {
         if item.sell_in < 0 {
             item.quality = 0;
         }
-        return;
     } else if is_legendary {
         return;
-    }
-
-    if item.quality > 0 {
-        item.quality -= 1;
-    }
-
-    item.sell_in -= 1;
-
-    if item.sell_in < 0 {
+    } else {
         if item.quality > 0 {
             item.quality -= 1;
+        }
+
+        item.sell_in -= 1;
+
+        if item.sell_in < 0 {
+            if item.quality > 0 {
+                item.quality -= 1;
+            }
         }
     }
 }
